8c5978fd2a0403a3699cffd7dc3d68c989fbd355
Adina Scheinfeld <Adina.Scheinfeld20@bcmail.cuny.edu>
2021-01-25 10:41:35 -0500

    arranges pages

---
+++ src/components/AddCampus.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddCampus extends Component {
+    render() { 
+        return ( 
+            <div>
+                <h1>Add Campus here</h1>
+            </div>
+         );
+    }
+}
+ 
+export default AddCampus;
